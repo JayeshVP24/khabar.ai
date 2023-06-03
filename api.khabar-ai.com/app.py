68a7df0b3b1fda2685fb048b40bc726c38cfb79e
hrishikesh332 <hriskikesh.yadav332@gmail.com>
2023-06-03 22:39:06 +0530

    Added Route 1. To get the sementic links 2. LLM to check the factual accuracy and authenticity

--- api.khabar-ai.com/app.py
+++ api.khabar-ai.com/app.py
@@ -28,6 +28,20 @@ import os
 import llama_index
 from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import quote
+from serpapi import GoogleSearch
+import os
+
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+# from langchain.utilities import WikipediaAPIWrapper
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -42,6 +56,9 @@ print(os.getenv("HUGGINGFACE_API"))
 print(os.getenv('OPENAI_API_KEY'))
 print(os.getenv('BEARER_TOKEN'))
 
+
+os.environ['OPENAI_API_KEY']= os.getenv('OPENAI_API_KEY')
+os.environ['SERPAPI_API_KEY']= os.getenv('apikey2')
 
 @app.route('/')
 def hello_geek():
@@ -88,6 +105,67 @@ def twitter():
     print(tweets)
     return jsonify({'result':tweets})
 
+#For getting the realted link - by providing the URL
+@app.route('/search', methods=['GET'])
+def search():
+    article_url = request.args.get('url')
+    response = requests.get(article_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    header = soup.find('h1').text.strip()
+    search_query = quote(header)
+
+    params = {
+        'q': search_query,
+        'hl': 'en',
+        'gl': 'us',
+        'api_key': os.environ.get('apikey2')
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict().get('organic_results', [])
+
+    links = [result['link'] for result in results]
+
+    return jsonify({'article_header': header, 'related_links': links})
+
+# To use LLM to check the factual accuracy of the news
+@app.route('/classify_news', methods=['GET'])
+def classify_news():
+
+    prompt = request.args['url']
+
+    tool_names = ["serpapi"]
+    tools = load_tools(tool_names)
+
+    title_template = PromptTemplate(
+    input_variables = ['topic'], 
+    template='To classify the news: {topic} in to the categories like murder, fire, accident, natural disaster, etc'
+)
+
+#     script_template = PromptTemplate(
+#     input_variables = ['title', 'wikipedia_research'], 
+#     #template='Look for the authenticity and the accuracy of the news listed: {title} provide the explanation of whether it is factually correct or is there any information present on wikipedia and also provide the correct answer or result if there is:{wikipedia_research} '
+#     template='Please verify the authenticity and accuracy of the news provided in the {title} by cross-referencing it with the corresponding {wikipedia_research} page. Examine the information available on Wikipedia and determine whether the news is factually correct or accurate. Additionally, if there is any conflicting or misleading information, please provide the correct answer or result based on your research from Wikipedia. '
+# )
+
+    title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+    # script_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+
+    llm = OpenAI(temperature=0.9) 
+    title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
+    # script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
+    agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
+    # wiki = WikipediaAPIWrapper()
+    
+    if prompt: 
+        title = title_chain.run(prompt)
+        a=agent.run(f"{prompt}. Also, provide the realted links")
+
+    return {
+        'title': title_memory.buffer,
+        'script': a
+    }
 
 @app.route('/xyz')
 def xyz():
